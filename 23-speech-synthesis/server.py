b6c3779e14f240f793774cc90d9bc6cdbf99b851
Liam Sean Brady <liamseanbrady@gmail.com>
2017-02-23 18:38:05 +0000

    My attempt at using speech synthesis

---
+++ 23-speech-synthesis/server.py
@@ -0,0 +1,18 @@
+def app(environ, start_response):
+  path = environ.get("PATH_INFO")
+  print(path)
+  if (path == "/favicon.ico"):
+    data = "Nope"
+    start_response("200 OK", [("Content-Type", "text/plain"), ("Content-Length", str(len(data)))])
+  elif (path == "/"):
+    f = open("index.html")
+    data = f.read()
+    f.close()
+    start_response("200 OK", [("Content-Type", "text/html"), ("Content-Length", str(len(data)))])
+  elif (path == "/style.css"):
+    f = open("style.css")
+    data = f.read()
+    f.close()
+    start_response("200 OK", [("Content-Type", "text/css"), ("Content-Length", str(len(data)))])
+
+  return iter([data])
